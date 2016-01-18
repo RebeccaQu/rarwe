6ecea6c704c8e73ed844d44da57778a308c983bc
Rebecca Qu <beks.qu@gmail.com>
2016-01-18 15:17:39 -0500

    prompt user to create a song after creating a band

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,11 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  noSongs: Ember.computed('model.songs.length', function() {
+    return this.get('model.songs.length') === 0;
+  }),
+  
   actions: {
     updateRating: function(params) {
       var song = params.item;
       var rating = params.rating;
-      
+
       song.set('rating', rating);
     }
   }
