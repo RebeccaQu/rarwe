e67cbeff20954e83ebb6c4ce42705a0ea508f016
Rebecca Qu <beks.qu@gmail.com>
2016-01-18 10:36:38 -0500

    added forms to create new band and songs

--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -3,5 +3,17 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.modelFor('bands.band');
+  },
+
+  actions: {
+    createSong: function() {
+      var controller = this.get('controller');
+      var band = this.modelFor('bands.band');
+      var title = controller.get('title');
+
+      var song = Song.create ({ title: title, band: band });
+      band.get('songs').pushObject(song);
+      controller.set('title', '');
+    }
   }
 });
