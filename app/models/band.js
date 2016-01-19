ac62c5088f7a519fe7a5527225ff826b8892596f
Rebecca Qu <beks.qu@gmail.com>
2016-01-19 10:17:12 -0500

    modified model band to use ember data

--- app/models/band.js
+++ app/models/band.js
@@ -1,16 +1,29 @@
-// BAND OBJECT
-export default Ember.Object.extend({
-  name: '',
-  description: '',
+import DS from 'ember-data';
 
-  // create a new Band object with an empty array to hold songs
-  setupSongs: Ember.on('init', function() {
-    if (!this.get('songs')) {
-      this.set('songs', []);
-    }
-  }),
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  description: DS.attr(),
+  songs: DS.hasMany('songs'),
 
   slug: Ember.computed('name', function() {
     return this.get('name').dasherize();
-  })
+  }),
 });
+
+
+// // BAND OBJECT
+// export default Ember.Object.extend({
+//   name: '',
+//   description: '',
+//
+//   // create a new Band object with an empty array to hold songs
+//   setupSongs: Ember.on('init', function() {
+//     if (!this.get('songs')) {
+//       this.set('songs', []);
+//     }
+//   }),
+//
+//   slug: Ember.computed('name', function() {
+//     return this.get('name').dasherize();
+//   })
+// });
