09e027a21378af760a850e771d02dfb172858f82
Rebecca Qu <beks.qu@gmail.com>
2016-01-19 10:19:18 -0500

    modified model song to use ember data

--- app/models/song.js
+++ app/models/song.js
@@ -1,6 +1,14 @@
-// SONG OBJECT
-export default Ember.Object.extend({
-  title: '',
-  band: null,
-  rating: 0,
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr('string'),
+  rating: DS.attr('number'),
+  band: DS.belongsTo('band'),
 });
+
+// // SONG OBJECT
+// export default Ember.Object.extend({
+//   title: '',
+//   band: null,
+//   rating: 0,
+// });
